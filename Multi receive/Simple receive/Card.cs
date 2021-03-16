633a3f0842e164dbd59b9b63ca1c4c5c218d8384
Jampo22 <jan.supper@gmx.de>
2021-03-16 16:20:49 +0100

    basic Card class with img import
    img import does not work yet. the image class does not contain a fromFile class?!

---
+++ Multi receive/Simple receive/Card.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Controls;
+
+namespace Simple_receive
+{
+    class Card
+    {
+       private int value;
+       private string colour;
+       private Image img;
+      
+
+        public Card(int value, string colour)
+        {
+            this.colour = colour;
+            this.value = value;
+            img = setImg();
+        
+        }
+
+        private Image setImg()
+        {
+            string filename = colour + value;
+            
+            Image temp = Image.FromFile(filename);
+
+            //search for file in folder where all pngs sit: eg heart7
+            //set img to found file in folder
+            return temp;
+
+        }
+
+    }
+}
