fe4569886f36fa743b5d06d55e971ba4bd4ccfb1
Melvin <melvin@vertech.de>
2021-03-16 14:28:21 +0100

    k

---
+++ Simple receive/Simple receive/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Simple_receive
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+        int Port = 8000;
+        TcpListener host = null;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void connect_Click(object sender, RoutedEventArgs e)
+        {
+            textBlock.Text = "";
+            Dispatcher.Invoke(() => { textBlock.Text = ""; });
+            Thread worker = new Thread(getResponse);
+            worker.Start();
+        }
+
+        private void getResponse()
+        {
+            try
+            {
+                host = new TcpListener(ipAddress, Port);
+                host.Start();
+
+                Byte[] bytes = new Byte[256];
+                String data = null;
+
+                while (true)
+                {
+                    Dispatcher.Invoke(() => { textBlock.Text += "\nVerbinde..."; });
+                    TcpClient client = host.AcceptTcpClient();
+                    Dispatcher.Invoke(() => { textBlock.Text += "\nVerbunden..."; });
+                    data = null;
+                    NetworkStream stream = client.GetStream();
+                    int i;
+                    while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                    {
+                        data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                        Dispatcher.Invoke(() => { textBlock.Text += "\n" + data; });
+
+                        data = data.ToUpper();
+
+                        byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+
+
+                    }
+                }
+
+            }catch(Exception e)
+            {
+                Dispatcher.Invoke(() => { textBlock.Text += "\n Exception: " + e; });
+
+            }
+            finally
+            {
+                host.Stop();
+            }
+        }
+    }
+}
---
+++ Simple send/Simple send/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Simple_send
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        String ServerIp = "127.0.0.1";
+        int Port = 8000;
+        TcpClient client = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void connect_btn_Click(object sender, RoutedEventArgs e)
+        {
+            if(client == null)
+            {
+                client = new TcpClient();
+                client.Connect(ServerIp, Port);
+            }
+        }
+
+        private void send_btn_Click(object sender, RoutedEventArgs e)
+        {
+            Thread worker = new Thread(sendPerThread);
+            worker.Start();
+        }
+
+        private void sendPerThread()
+        {
+            
+            string toSend = "";
+            Dispatcher.Invoke(() => { toSend = textBox.Text; });
+            byte[] buffer = ASCIIEncoding.UTF8.GetBytes(toSend);
+
+            NetworkStream nwStream = client.GetStream();
+            nwStream.Write(buffer, 0, buffer.Length);
+        }
+
+        private void disconnect_btn_Click(object sender, RoutedEventArgs e)
+        {
+            if (client != null)
+            {
+                client.Close();
+                client = null;
+            }
+        }
+    }
+}
