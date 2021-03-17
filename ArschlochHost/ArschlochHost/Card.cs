9c8cbf4328c533dd6cfec44ab900fec0027baca6
Jampo22 <jan.supper@gmx.de>
2021-03-17 14:14:27 +0100

    copied to right proj

---
+++ ArschlochHost/ArschlochHost/Card.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Controls;
+
+namespace ArschlochHost
+{
+    class Card
+    {
+        private int value;
+        private string colour;
+        private Image img;
+        private static int counter = 0;
+
+        public int id { get; set; }
+        public bool handedOut;
+
+
+        public Card(int value, string colour)
+        {
+            this.colour = colour;
+            this.value = value;
+            img = setImg();
+            this.id = System.Threading.Interlocked.Increment(ref counter);
+            handedOut = false;
+
+        }
+
+        private Image setImg()
+        {
+
+            //search for file in folder where all pngs sit: eg heart7
+            //set img to found file in folder
+            string filename = colour + value;
+
+            //Image temp = Image.FromFile(filename);
+
+            Image temp = new Image();
+            return temp;
+
+        }
+
+    }
+}
